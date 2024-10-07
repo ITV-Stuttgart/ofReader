[BugFix] In computation of cell center and add volume calculation

ofReader/fvMesh.py
@@ -14,6 +14,7 @@ class fvMesh:
         self._neighbor = readOpenFOAMFile(casePath + '/constant/polyMesh/neighbour')
         
         self._centers = []
+        self._volumes = []
 
         # Get the number of cells
         nCells = 0
@@ -47,6 +48,14 @@ class fvMesh:
             for i in range(len(self._cells)):
                 self._centers[i] = self._cells[i].midPoint(self._points,self._faces)
         return self._centers
+    
+    def volumes(self):
+        if len(self._volumes) == 0:
+            # Calculate the centers
+            self._volumes = np.zeros(len(self._cells))
+            for i in range(len(self._cells)):
+                self._volumes[i] = self._cells[i].volume(self._points,self._faces)
+        return self._volumes
 
 class fvmCell:
     """Container to store the information of a cell and function to 
@@ -56,6 +65,7 @@ class fvmCell:
         self._faceList = []
         self._midPoint = np.zeros(3)
         self._midPointSet = False
+        self._volume = 0
 
     def addFaceIndex(self,faceIndex):
         self._faceList.append(faceIndex)
@@ -67,14 +77,51 @@ class fvmCell:
         This is incorrect but gives an idea
         """
         if not self._midPointSet:
+            nPoints = 0
             for faceIndex in self._faceList:
                 for pointIndex in faces[faceIndex]:
                     self._midPoint = self._midPoint + points[pointIndex]
-            self._midPoint = self._midPoint/len(self._faceList)
+                    nPoints = nPoints + 1
+            self._midPoint = self._midPoint/nPoints
             self._midPointSet = True
             return self._midPoint
         else:
             return self._midPoint
+        
+    def volume(self,points,faces):
+        # Calculate the mid point
+        self.midPoint(points,faces)
+        self._volume = 0
+        # Decompose cell into tetrahedars for each face 
+        for faceIndex in self._faceList:
+            face = faces[faceIndex]
+            # Always split face in tets
+            if len(face) == 4:
+                # Calculate the volume based on tet1
+                vec1 = points[face[0]]-self._midPoint
+                vec2 = points[face[1]]-self._midPoint
+                vec3 = points[face[2]]-self._midPoint
+ 
+                M1 = np.reshape(np.concatenate((vec1,vec2,vec3)),(3,3))
+                self._volume = self._volume + np.abs(1.0/6.0*np.linalg.det(M1))
+
+                # Calculate the volume based on tet2
+                vec1 = points[face[2]]-self._midPoint
+                vec2 = points[face[3]]-self._midPoint
+                vec3 = points[face[0]]-self._midPoint
+                M2 = np.reshape(np.concatenate((vec1,vec2,vec3)),(3,3))
+                
+                self._volume = self._volume + np.abs(1.0/6.0*np.linalg.det(M2))
+            if len(face) == 3:
+                # Calculate the volume based on tet1
+                vec1 = points[face[0]]-self._midPoint
+                vec2 = points[face[1]]-self._midPoint
+                vec3 = points[face[2]]-self._midPoint
+                
+                M1 = np.reshape(np.concatenate((vec1,vec2,vec3)),(3,3))
+                self._volume = self._volume + np.abs(1.0/6.0*np.linalg.det(M1))
+        
+        return self._volume
 
 
 

tests/test_fvMesh.py
@@ -1,13 +1,23 @@
 from ofReader.fvMesh import fvMesh
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 mesh = fvMesh('tests/testCase')
 
 centers = np.array(mesh.centers())
 
-# fig = plt.figure()
-# ax = fig.add_subplot(projection='3d')
-# ax.scatter(centers[:,0],centers[:,1],centers[:,2])
-# plt.savefig('test.png',format='png')
+
+V =mesh.volumes()
+# Add up all cell volumes, must equal 1.0
+totalVolume = 0
+for e in V:
+    totalVolume = totalVolume + e
+
+assert math.isclose(totalVolume,1.0)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(centers[:,0],centers[:,1],centers[:,2])
+plt.savefig('test.png',format='png')
 