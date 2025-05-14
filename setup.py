f12ae59040b2b2a9a273bc4fed8a6133c3e7f6d7
Jan Gaertner <jan-wilhelm.gaertner@itv.uni-stuttgart.de>
2025-05-14 10:16:14 +0200

    Add pyvista to required packages in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Library to read OpenFOAM data into python for post processing',
     author='Jan Wilhelm Gaertner',
     license='GNUv3',
-	install_requires=['numpy','tqdm','scipy'],
+	install_requires=['numpy','tqdm','scipy','pyvista'],
 	setup_requires=['pytest-runner'],
 	tests_require=['pytest==6.2.5'],
 	test_suite='tests',
